f9786512e404842602492153b0ed1dc35dd84db6
gbhoang <gb.hoang02@gmail.com>
2022-05-06 00:11:20 +0930

    task 1.8

--- prac6.js
+++ prac6.js
@@ -68,7 +68,12 @@ var vueinst = new Vue({
         c_text: "type your comment here",
         c_arr: [],
         top_menu_item: 0,
-        top_menu_hover: null,
+        top_menu_hover: false,
+        s_arr: [
+            { name:'Adelaide City',  address:'45 Florabunda Lane, Adelaide, 5000', counter: 0, img:'https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/11_Gloddaeth_Street%2C_Llandudno_shop_front.jpg/320px-11_Gloddaeth_Street%2C_Llandudno_shop_front.jpg' },
+            { name:'Steelton South', address:'77 Weigall Avenue, Steelton, 5413',  counter: 0, img:'https://upload.wikimedia.org/wikipedia/commons/4/42/Well-shop-front.jpg' },
+            { name:'Milton',         address:'33 McGregor Street, Milton, 5880',   counter: 0, img:'https://upload.wikimedia.org/wikipedia/en/thumb/e/e2/Greggs_store_front.JPG/320px-Greggs_store_front.JPG' }
+        ],
     },
     methods: {
         add_comment: function(event) {
